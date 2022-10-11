abdee6fe0789d94f2620b1a299e92ad84ce1ec97
Ellie <escrypto19@gmail.com>
2022-10-11 11:14:15 -0400

    Form validates, still missing email format

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React from "react";
-// TODO: import useFormik from formik library
+import { useFormik } from 'formik';
+
+import './index.css';
 
 function App() {
   // TODO: add a const called formik assigned to useFormik()
-
+const formik = useFormik({
+  initialValues: {
+    email: '',
+    password: '',
+  },
+  onSubmit: values => {
+    console.log('form', values);
+  },
+  validate: values => {
+    let errors = {};
+    if (!values.email) errors.email = 'Field required';
+    if (!values.password) errors.password = 'Field required';
+    return errors;
+  }
+});
   return (
     <div>
-      <p>
-        The app is ready! You can proceed with the task instructions. TODO:
-        build you form here.
-      </p>
+    <form onSubmit={formik.handleSubmit}>
+     <div>Email:</div>
+        <input type="text" id="emailField" name="email" onChange={formik.handleChange} value={formik.values.email}/>
+        {formik.errors.email ? <div id="emailError" style={{color:'red'}}>{formik.errors.email}</div> : null}        
+        <div>Password:</div>
+        <input type="text" id="pswField" name="password" onChange={formik.handleChange} value={formik.values.password}/><br/>
+        {formik.errors.password ? <div id="pswError" style={{color:'red'}}>{formik.errors.password}</div> : null}                
+        <button id="submitBtn" type="submit">Submit</button>
+    </form>
     </div>
   );
 }
