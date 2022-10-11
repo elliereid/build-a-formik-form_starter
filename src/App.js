5e0085d3affef92855a321a946e56c6fd33f2cf1
Ellie <escrypto19@gmail.com>
2022-10-11 11:52:33 -0400

    Add validates

--- src/App.js
+++ src/App.js
@@ -11,11 +11,19 @@ const formik = useFormik({
     password: '',
   },
   onSubmit: values => {
-    console.log('form', values);
+    alert("Login Successful");
   },
   validate: values => {
     let errors = {};
-    if (!values.email) errors.email = 'Field required';
+      if (!values.email) {
+
+     errors.email = 'Field required';
+
+   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+
+     errors.email = 'Username should be an email';
+
+   }
     if (!values.password) errors.password = 'Field required';
     return errors;
   }
